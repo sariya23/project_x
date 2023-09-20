a4452a44a3349ec77906427c8671715c9146f469
nikita <dayzwaitik123@yandex.ru>
2023-09-20 12:26:49 +0300

    Pass js code in file

---
+++ static/send_form.js
@@ -0,0 +1,24 @@
+document.getElementById('form').addEventListener('submit', function(event) {
+        event.preventDefault()
+        const inputAmountCurrencyFrom = document.getElementById('amount_currency_from').value;
+        const SelectCurrencyFrom = document.getElementById('dropdown_currency_from').value;
+        const SelectCurrencyTo = document.getElementById('dropdown_currency_to').value;
+
+        const formData = new FormData();
+        formData.append('inputAmountCurrencyFrom', inputAmountCurrencyFrom);
+        formData.append('SelectCurrencyFrom', SelectCurrencyFrom);
+        formData.append('SelectCurrencyTo', SelectCurrencyTo);
+
+        fetch('/converted_values', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('result_text').textContent = `Из ${data.amount_from_currency} ${data.from_currency} получается ${data.converted_amount}${data.to_currency}`;
+            resultDiv.style.display = 'block';
+        })
+        .catch(error => {
+            console.error('Ошибка:', error);
+        });
+});
