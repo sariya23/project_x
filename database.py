633db88ab9ffb07b382b09b2179fdeb3c0b85477
lomowg <sokol789123@yandex.ru>
2023-09-21 00:53:13 +0300

    added __init__

--- database.py
+++ database.py
@@ -1,3 +1,8 @@
+import sqlite3
+
+
 class DataBase:
-    def __init__(self):
-        pass
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self._connection = sqlite3.connect(db_name)
+
